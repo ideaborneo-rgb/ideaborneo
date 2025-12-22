749547ad1cae731ea14e074fc3c93b64809315db
ideaborneo-rgb <ideaborneo@gmail.com>
2025-12-22 10:09:23 +0700

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -82,7 +82,7 @@ export const AboutSection = () => {
                   />
                   {/* konten */}
                   <div className="relative z-10 w-full h-full flex items-center justify-center">
-                    <span className="text-4xl">🏠</span>
+                    <span className="text-4xl">test</span>
                   </div>
                 </div>
 
