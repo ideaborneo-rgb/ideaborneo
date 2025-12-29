a770b1dae0f2c322be9c743348543ddb194d598d
ideaborneo-rgb <ideaborneo@gmail.com>
2025-12-29 13:12:48 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: "/ideaborneo/",
+  base: "/ideaborneo.com/",
   plugins: [react()],
   resolve: {
     alias: {
